758d13ddc00837cdd6200330dd7cda93c4d317c2
Shreyas Prasad <sprasad@cohenandsteers.com>
2022-06-10 15:35:54 -0400

    Commit everything actually

---
+++ API/TestAppAPI/TestAppAPI/Controllers/AnswerController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestAppAPI.Models;
+using TestAppAPI.Services;
+
+namespace TestAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnswerController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<Answer> Get(int id)
+        {
+            Answer? res = AnswerService.Get(id);
+
+            if (res == null) return NotFound();
+
+            return Ok(res);
+        }
+
+        [HttpGet("question/{questionId}")]
+        public ActionResult<List<Answer>> GetAnswers(int questionId)
+        {
+            List<Answer> res = AnswerService.GetAnswers(questionId);
+
+            if (res.Count == 0) return NotFound();
+
+            return Ok(res);
+        }
+
+        [HttpPost]
+        public IActionResult AddAnswer(Answer answer)
+        {
+            int id = AnswerService.Add(answer);
+            return CreatedAtAction("Get", new { id }, answer);
+        }
+    }
+}
---
+++ API/TestAppAPI/TestAppAPI/Services/AnswerService.cs
@@ -0,0 +1,31 @@
+﻿using TestAppAPI.Models;
+
+namespace TestAppAPI.Services
+{
+    public class AnswerService
+    {
+        private static List<Answer> s_answers;
+        private static int s_nextId = 0;
+
+        static AnswerService()
+        {
+            s_answers = new List<Answer>
+            {
+                new Answer {Id = 0, QuestionId = 0, Text = "easy fix", AnsBy = "Everyone", AnsAt = "Jan 02 2001 @ 21:00:02" }
+            };
+        }
+
+        public static List<Answer> GetAll() => s_answers;
+
+        public static List<Answer> GetAnswers(int questionId) => s_answers.Where(ans => ans.QuestionId == questionId).ToList();
+
+        public static Answer? Get(int id) => s_answers.FirstOrDefault(ans => ans.Id == id);
+
+        public static int Add(Answer answer)
+        {
+            answer.Id = s_nextId++;
+            s_answers.Add(answer);
+            return answer.Id;
+        }
+    }
+}
